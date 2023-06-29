6af4180ee00cf8781abad653608db0b0694115b7
Nicholas Gilbert <nicholasgilbert21@yahoo.com>
2023-06-29 01:30:54 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nicholas Gilbert on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
